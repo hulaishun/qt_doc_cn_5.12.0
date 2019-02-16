754ec7f26a8b47c9a52180d5d90c8805547cd4da
hulaishun <hulaishun@yiwei.com>
2019-02-16 23:11:24 +0800

    支持单个目录或所有目录的字典库英译中的替换

--- TranslateTool/src/En2Cn/En2CnKeyForm.cpp
+++ TranslateTool/src/En2Cn/En2CnKeyForm.cpp
@@ -32,6 +32,8 @@ void CEn2CnKeyForm::InitAllControl()
     connect(ui->pbtnImportKey, &QPushButton::clicked, this, &CEn2CnKeyForm::OnImportKeyClicked);
     connect(ui->pbtnExportKey, &QPushButton::clicked, this, &CEn2CnKeyForm::OnExportKeyClicked);
 	connect(ui->pbtnTranslateFile, &QPushButton::clicked, this, &CEn2CnKeyForm::OnTranslateFileClicked);
+	connect(ui->pbtnTranslatePath, &QPushButton::clicked, this, &CEn2CnKeyForm::OnTranslatePathClicked);
+	connect(ui->pbtnTranslateAllPath, &QPushButton::clicked, this, &CEn2CnKeyForm::OnTranslateAllPathClicked);
     connect(ui->tableViewKey, &QTableView::clicked, this, &CEn2CnKeyForm::OnKeyClicked);
 
     ui->tableViewKey->setModel(gEn2CnMgr->GetKeyModel());
@@ -148,13 +150,118 @@ void CEn2CnKeyForm::OnKeyClicked(const QModelIndex& index)
 void CEn2CnKeyForm::OnTranslateFileClicked()
 {
 	QString strFile = QFileDialog::getOpenFileName(this, QStringLiteral("打开文件"), QCoreApplication::applicationFilePath(), QStringLiteral("英文文档 (*.html *.htm);;所有文档 (*.*)"));
+
+	TranslateFile(strFile);	
+}
+
+void CEn2CnKeyForm::OnTranslatePathClicked()
+{
+	QString strPath = QFileDialog::getExistingDirectory(this, QStringLiteral("打开目录"), QCoreApplication::applicationFilePath(), QFileDialog::ShowDirsOnly|QFileDialog::DontResolveSymlinks);
+
+	TranslatePath(strPath);
+}
+
+
+void CEn2CnKeyForm::OnTranslateAllPathClicked()
+{
+	QString strPath = QFileDialog::getExistingDirectory(this, QStringLiteral("打开目录"), QCoreApplication::applicationFilePath(), QFileDialog::ShowDirsOnly | QFileDialog::DontResolveSymlinks);
+
+	QDir dir(strPath + "/activeqt");
+	if (!dir.exists())
+	{
+		return;
+	}
+
+	QStringList listPath;
+	listPath << strPath + "/activeqt";
+	listPath << strPath + "/global";
+	listPath << strPath + "/qdoc";
+	listPath << strPath + "/qmake";
+	listPath << strPath + "/qt3d";
+	listPath << strPath + "/qtandroidextras";
+	listPath << strPath + "/qtbluetooth";
+	listPath << strPath + "/qtcharts";
+	listPath << strPath + "/qtconcurrent";
+	listPath << strPath + "/qtcore";
+	listPath << strPath + "/qtdatavisualization";
+	listPath << strPath + "/qtbus";
+	listPath << strPath + "/qtdoc";
+	listPath << strPath + "/qtgamepad";
+	listPath << strPath + "/qtgraphicaleffects";
+	listPath << strPath + "/qtgui";
+	listPath << strPath + "/qthelp";
+	listPath << strPath + "/qtimageformats";
+	listPath << strPath + "/qtlabscalendar";
+	listPath << strPath + "/qtlabsplatform";
+	listPath << strPath + "/qtlocation";
+	listPath << strPath + "/qtmacextras";
+	listPath << strPath + "/qtmultimedia";
+	listPath << strPath + "/qtnetwork";
+	listPath << strPath + "/qtnetworkauth";
+	listPath << strPath + "/qtnfc";
+	listPath << strPath + "/qtplatformheaders";
+	listPath << strPath + "/qtpositioning";
+	listPath << strPath + "/qtprintsupport";
+	listPath << strPath + "/qtpurchasing";
+	listPath << strPath + "/qtqml";
+	listPath << strPath + "/qtquick";
+	listPath << strPath + "/qtquickcontrols";
+	listPath << strPath + "/qtquickdialogs";
+	listPath << strPath + "/qtquickextras";
+	listPath << strPath + "/qtremoteobjects";
+	listPath << strPath + "/qtscxml";
+	listPath << strPath + "/qtsensors";
+	listPath << strPath + "/qtserialbus";
+	listPath << strPath + "/qtserialport";
+	listPath << strPath + "/qtspeech";
+	listPath << strPath + "/qtsql";
+	listPath << strPath + "/qtsvg";
+	listPath << strPath + "/qtvirtualkeyboard";
+	listPath << strPath + "/qtwaylandcompositor";
+	listPath << strPath + "/qtwebchannel";
+	listPath << strPath + "/qtwebengine";
+	listPath << strPath + "/qtwebsockets";
+	listPath << strPath + "/qtwebview";
+	listPath << strPath + "/qtwidgets";
+	listPath << strPath + "/qtx11extras";
+	listPath << strPath + "/qtxml";
+	listPath << strPath + "/qtxmlpatterns";
+
+	foreach(QString strSubPath, listPath)
+	{
+		TranslatePath(strSubPath);
+	}
+}
+
+void CEn2CnKeyForm::TranslatePath(const QString& strPath)
+{
+	if (strPath.isEmpty())
+	{
+		return;
+	}
+
+	QDir dirPath(strPath);
+	QStringList listSuffix;
+	listSuffix << "*.html" << "*.htm";
+	dirPath.setNameFilters(listSuffix);
+
+	QStringList listFile = dirPath.entryList(QDir::Files|QDir::Readable, QDir::Size | QDir::Time | QDir::Name);
+
+	foreach(QString strFile, listFile)
+	{
+		TranslateFile(QString("%1/%2").arg(strPath).arg(strFile));
+	}
+}
+
+void CEn2CnKeyForm::TranslateFile(const QString& strFile)
+{
 	if (strFile.isEmpty())
 	{
 		return;
 	}
 
 	QFile oldFile(strFile);
-	if (!oldFile.open(QIODevice::ReadWrite|QIODevice::Text))
+	if (!oldFile.open(QIODevice::ReadWrite | QIODevice::Text))
 	{
 		return;
 	}
@@ -163,7 +270,7 @@ void CEn2CnKeyForm::OnTranslateFileClicked()
 	QString strOldData = instream.readAll();
 	oldFile.close();
 
-	foreach (CEn2CnKeyInfo keyinfo, gEn2CnMgr->GetKeyModel()->GetDataList())
+	foreach(CEn2CnKeyInfo keyinfo, gEn2CnMgr->GetKeyModel()->GetDataList())
 	{
 		strOldData.replace(keyinfo.GetEnKey().toUtf8(), keyinfo.GetCnKey().toUtf8());
 	}
@@ -177,5 +284,4 @@ void CEn2CnKeyForm::OnTranslateFileClicked()
 	outstream.setCodec("UTF-8");
 	outstream << strOldData;
 	newFile.close();
-	
 }
--- TranslateTool/src/En2Cn/En2CnKeyForm.h
+++ TranslateTool/src/En2Cn/En2CnKeyForm.h
@@ -23,12 +23,16 @@ private slots:
     void OnImportKeyClicked();
     void OnExportKeyClicked();
 	void OnTranslateFileClicked();
+	void OnTranslatePathClicked();
+	void OnTranslateAllPathClicked();
 
     void OnKeyClicked(const QModelIndex& index);
 
 private:
     void InitAllControl();
     void ClearListView();
+	void TranslatePath(const QString& strPath);
+	void TranslateFile(const QString& strFile);
 
 private:
     Ui::CEn2CnKeyForm *ui;
